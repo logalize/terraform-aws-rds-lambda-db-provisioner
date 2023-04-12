a0acfce97193177be395d503b14023c52a77e24b
Vladislav Markov <logalize@gmail.com>
2023-04-12 14:30:59 +0300

    initial commit

---
+++ versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 0.13.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.1.0"
+    }
+  }
+}
